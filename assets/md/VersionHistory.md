44b32fd100dc1e59e9abb5188cf0c1b779428f1d
My Name <myemail@example.com>
2023-12-07 12:06:34 +0800

    V0.0.2.1 (Enhence Styling)

--- assets/md/VersionHistory.md
+++ assets/md/VersionHistory.md
@@ -6,29 +6,31 @@
 
 <br/>
 
-#### V0.0.2 [2023-12-06]
 ## Filter Function
+#### V0.0.2.1 [2023-12-07]
+### Changed
+- Enhancement of Index page styling
 
+<br/>
+
+#### V0.0.2 [2023-12-06]
 ### Completed
 - Filter Function
-
 ### Repaired Bugs
 - Tasks could not be set.  
 - The redirecting page New cannot be displayed.
 
 <br/>
 
-#### V0.0.1 [2023-12-05]
 ## Recursive Tree
-
+#### V0.0.1 [2023-12-05]
 ### Changed
 - Integrated recursive tree.
 
 <br/>
 
-#### V0 [2023-12-04]
 ## Pre-Alpha Version
-
+#### V0 [2023-12-04]
 ### Features
 - Homepage
 - Add, View, Edit, Delete Properties and Timelogs
--- index.php
+++ index.php
@@ -20,29 +20,89 @@ function result_plan($order_by = '') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/index.css">
     <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/color.css">
+    <link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
+    <div class="header">
+        <div class="wrap-header">
+            <div class="logo"></div>
+            <ul>
+                <li>
+                    <div class="header-item">
+                        <a href="attr.php?m=c">查看分類</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="header-item">
+                        <a href="attr.php?m=t">查看任務</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="header-item">
+                        <a href="attr.php?m=l">查看標籤</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="header-item">
+                        <a href="event.php?r=1">新增計畫</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="header-item">
+                        <a href="event.php?r=0">新增記錄</a>
+                    </div>
+                </li>
+                <li>
+                    <div class="header-item">
+                        <a href="event.php?r=2">開始記錄</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div class="titlebar">
+            <div class="main-title">時間日誌</div>
+            <div class="sub-title">這是我的副標題</div>
+            <a href="#go" class="go">>>立刻開始</a>
+        </div>
+        <div id="go"></div>
+    </div>
+
     <div class="toolbar">
-        <div class="tbar">
-            <a href="attr.php?m=c">查看分類</a>
-        </div>
-        <div class="tbar">
-            <a href="attr.php?m=t">查看任務</a>
-        </div>
-        <div class="tbar">
-            <a href="attr.php?m=l">查看標籤</a>
-        </div>
-        <div class="tbar">
-            <a href="event.php?r=1">新增計畫</a>
-        </div>
-        <div class="tbar">
-            <a href="event.php?r=0">新增記錄</a>
-        </div>
-        <div class="tbar">
-            <a href="event.php?r=2">開始記錄</a>
-        </div>
+        <ul>
+            <li>
+                <div class="tbar">
+                    <a href="attr.php?m=c">查看分類</a>
+                </div>
+            </li>
+            <li>
+                <div class="tbar">
+                    <a href="attr.php?m=t">查看任務</a>
+                </div>
+            </li>
+            <li>
+                <div class="tbar">
+                    <a href="attr.php?m=l">查看標籤</a>
+                </div>
+            </li>
+            <li>
+                <div class="tbar">
+                    <a href="event.php?r=1">新增計畫</a>
+                </div>
+            </li>
+            <li>
+                <div class="tbar">
+                    <a href="event.php?r=0">新增記錄</a>
+                </div>
+            </li>
+            <li>
+                <div class="tbar">
+                    <a href="event.php?r=2">開始記錄</a>
+                </div>
+            </li>
+        </ul>
     </div>
 
     <div class="container">
